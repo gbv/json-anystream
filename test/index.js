d3de6a7f346d901e3426f2171ac8d6ad7e453231
Stefan Peters <Stefan.Peters@gbv.de>
2024-09-23 13:17:07 +0200

    Test exported properties by index.js

--- test/index.js
+++ test/index.js
@@ -1,1 +1,21 @@
-// TODO: Tests whether all expected properties are exported in index.js
+import assert from "node:assert"
+import * as anystream from "../src/index.js"
+
+const properties = [
+  "make",
+  "convert",
+  "addStream",
+  "MissingTypeError",
+  "InvalidOrMissingDataFieldError",
+  "UrlRequestError",
+  "UnexpectedNonObjectValueError",
+  "StreamAnyObject",
+]
+
+describe("index.js", () => {
+  for (const property of properties) {
+    it(`should export property ${property}`, () => {
+      assert(!!anystream[property], `Property ${property} is not exported by index.js.`)
+    })
+  }
+})
