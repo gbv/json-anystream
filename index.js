2e6a6084b991ebd7a0d8b856863d51a100aceddc
Stefan Peters <Stefan.Peters@gbv.de>
2024-09-23 13:10:11 +0200

    Fix issue in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export * from "./src/StreamAnyObject.js"
+export { default as StreamAnyObject } from "./src/StreamAnyObject.js"
 export * from "./src/anystream.js"
 export * from "./src/middleware.js"
 export * from "./src/errors.js"
