b0aa950999be52f48d1d489968a846030296b92f
Stefan Peters <Stefan.Peters@gbv.de>
2024-09-23 13:11:45 +0200

    Move index.js to src/

--- index.js
+++
@@ -1,4 +0,0 @@
-export { default as StreamAnyObject } from "./src/StreamAnyObject.js"
-export * from "./src/anystream.js"
-export * from "./src/middleware.js"
-export * from "./src/errors.js"
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as StreamAnyObject } from "./StreamAnyObject.js"
+export * from "./anystream.js"
+export * from "./middleware.js"
+export * from "./errors.js"
